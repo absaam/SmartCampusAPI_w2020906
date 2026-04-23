55cf51036905a287c328e172546a0ea044938fea
absaam <w2020906@westminster.ac.uk>
2026-04-23 02:21:44 +0100

    Fix discovery endpoint path to /api/v1

--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("discover")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class DiscoveryResource {
 
